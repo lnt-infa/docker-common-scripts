3e1bade43037b3c5800b5dca64a70802811fd541
Louis-Noël Trapadoux <ltrapadoux@informatica.com>
2018-04-14 07:23:40 +0200

    update consul functions

--- consulFunctions.sh
+++ consulFunctions.sh
@@ -12,7 +12,7 @@ function consul_update_dns() {
   if [ `echo $n | wc` -ge 5 ]; then
     ip_old=`echo $n | python -c "import sys, json; print json.load(sys.stdin)['Node']['Address']"`
   fi
-  ip_new=`hostname -I | tr -d " "`
+  ip_new=`hostname -I | awk -F" " '{print($1)}' |tr -d " "`
  
   if [ "${ip_old}" != "${ip_new}" ]; then
     if [ `echo $n | wc` -ge 5 ]; then
